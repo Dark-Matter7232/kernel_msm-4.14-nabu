7881dd70923cb64ffdbec8278cebf478716b3794
Dark-Matter7232 <me@const.eu.org>
2023-09-02 02:47:05 +0530

    nabu: import KernelSU
    Change-Id: I762b75ee9907ae096a923391a0e888a73942e08d
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- fs/exec.c
+++ fs/exec.c
@@ -1737,6 +1737,13 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1750,6 +1757,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
