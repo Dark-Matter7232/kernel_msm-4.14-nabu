fb80d87d010b627289d34d42e3eb31dd6a390b53
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-11 22:37:40 +0200

    drivers: input: touchscreen: tp_common: Add pen node
    Change-Id: I5d4046dba7a13d58cd78d2fafdc89e57e4747873
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- include/linux/input/tp_common.h
+++ include/linux/input/tp_common.h
@@ -13,3 +13,4 @@ struct tp_common_ops {
 int tp_common_set_capacitive_keys_ops(struct tp_common_ops *ops);
 int tp_common_set_double_tap_ops(struct tp_common_ops *ops);
 int tp_common_set_reversed_keys_ops(struct tp_common_ops *ops);
+int tp_common_set_pen_ops(struct tp_common_ops *ops);
